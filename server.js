1cd8e08d59a4ac5ba492ef6161ebad869e9e0188
dominicjunik <111475701+dominicjunik@users.noreply.github.com>
2023-08-17 13:56:02 -0600

    added the create route

--- server.js
+++ server.js
@@ -4,9 +4,31 @@ const app = express()
 
 const PORT = 8080
 
-app.get('/', (req, res) => {
-    res.send('new')
+const jsxEngine = require('jsx-view-engine')
+
+app.set('view engine', 'jsx')
+app.engine('jsx', jsxEngine())
+
+app.use(express.urlencoded({ extended: true }))
+// routes
+
+// index
+app.get('/logs', (req, res) => {
+    res.render('New')
 })
+
+// show
+// new
+// create
+app.post('/logs', (req, res) => {
+    req.body.shipIsBroken = (req.body.shipIsBroken === 'on') 
+    res.send(req.body)
+})
+
+// edit
+// update
+// destroy
+
 
 
 app.listen(PORT, console.log('Listening on port: ' + PORT, process.env.MONGO_URL))
